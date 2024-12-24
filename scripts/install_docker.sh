5d44373dd2aed1cf488c131d8848adcaa3c588b0
bharathac5775 <bharathac5775@gmail.com>
2024-12-25 01:47:33 +0530

    mompopweb db compose files are uploaded

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -8,7 +8,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Set up the stable repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
